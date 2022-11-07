6bfcff7b3c031d9d4cac9635dae9dab9dc19fcbc
Anson's laptop <Anson's laptop@DESKTOP-LDJSFOQ>
2022-11-06 21:18:34 -0700

    Created a new java project for lab 9

---
+++ src/java/database/userdb.sql
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Anson's laptop
+ * Created: 24-Oct-2022
+ */
+
